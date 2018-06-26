52ab48eea67f9eeff979a75043e92a6419f77e80
Jdy <jdy@JdydeMacBook-Air.local>
2018-06-26 19:05:27 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -4,14 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import 'styles/reset.css'
 // 解决1px边框
 import 'styles/border.css'
+// 引入图标库
 import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 // 解决移动端点击事件300ms延迟的问题
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
+        },
+        {
+          id: '0004',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+// 本来swiper这个属性是组件的属性这里是控制不了的，用>>>来穿透控制
+.wrapper >>> .swiper-pagination-bullet-active
+  background #ffffff
+.wrapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 31.25%
+  background #eee
+  .swiper-img
+    width 100%
+</style>
