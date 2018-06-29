33b5fa86d7507bcdb9d9af33e41a4f69a4535e9e
Jdy <jdy@JdydeMacBook-Air.local>
2018-06-29 08:30:54 +0800

    Performance optimization

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 路由页面执行过后就放在内存中，再次加载重复页面是直接从内存拿取，优化ajax请求次数 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
