7e8b03a35089e3d9097c359bab1f6d45d6cb08c2
Jdy <jdy@JdydeMacBook-Air.local>
2018-06-29 21:39:22 +0800

    goodnight

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+<script>
+import CommonGallery from 'common/gallery/Gallery'
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true
+    }
+  },
+  methods: {
+    handleScroll () {
+      console.log(document.documentElement.scrollTop);
+      
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs
+    position absolute
+    left .1rem
+    top .1rem
+    width .7rem
+    height .7rem
+    line-height .7rem
+    border-radius 50%
+    background rgba(0, 0, 0, .5)
+    text-align center
+    display flex
+    flex-direction column
+    justify-content center
+    .header-back
+      font-size .35rem
+      color #ffffff
+      font-weight bold
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right 0
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #ffffff
+    background $bgColor
+    font-size .32rem
+    .header-fixed-back
+      position absolute
+      width .64rem
+      top 0
+      left 0
+      text-align center
+      color #ffffff
+      font-size .4rem
+</style>
