821d6a32b90b482d7ea15360fe2add8e5537197a
MrJdy <jdy95270@gmail.com>
2018-07-02 15:09:01 +0800

    change json api

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8888'
+        target: 'http://123.59.116.174:8888'
       }
     },
 
